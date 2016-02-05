d2ff9f6a593ec5b7791008985c59c9f37f015c52
alu0100727296 <alu0100727296@ull.edu.es>
2016-02-05 18:18:54 +0000

    "delete.java funcionando"

--- LibreriaClases/src/file/delete.java
+++ LibreriaClases/src/file/delete.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package file;
-
+import java.io.File;
 /**
  *
  * @author julian Tanausu
  */
 public class delete {
-    
+    public static void main(String[] args) {
+        File f = null;
+           
+        try{
+            // create new file
+            f = new File("test.txt");
+           
+            // prints
+            System.out.println("File deleted: "+f.delete());
+         
+            // creates file in the system
+            f.createNewFile();
+        
+            // createNewFile() is invoked
+            System.out.println("createNewFile() method is invoked");
+         
+            // print
+            System.out.println("File deleted: "+f.delete());
+            
+        }catch(Exception e){
+            // if any error occurs
+            e.printStackTrace();
+        }
+    }
 }
