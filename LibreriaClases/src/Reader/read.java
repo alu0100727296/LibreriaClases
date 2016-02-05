fb6aa54a8cf8c275a55dfcfc2a60e785d44c0e34
alu0100727296 <alu0100727296@ull.edu.es>
2016-02-05 22:55:02 +0000

    "read.java funcionando"

---
+++ LibreriaClases/src/Reader/read.java
@@ -0,0 +1,34 @@
+package Reader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+/**
+ *
+ * @author julian Tanausu
+ */
+public class read {
+    public static void main(String[] args) {
+
+        String s = "Hello World";
+
+        // create a new StringReader
+        Reader reader = new StringReader(s);
+
+        try {
+            // read the first five chars
+            for (int i = 0; i < 5; i++) {
+                char c = (char) reader.read();
+                System.out.print("" + c);
+            }
+
+            // change line
+            System.out.println();
+
+            // close the stream
+            reader.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
