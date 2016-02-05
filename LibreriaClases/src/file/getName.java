f967513c933dcf99309fa243263670c5539bf762
alu0100727296 <alu0100727296@ull.edu.es>
2016-02-05 22:30:10 +0000

    "getName.java funcionando"

---
+++ LibreriaClases/src/file/getName.java
@@ -0,0 +1,46 @@
+package file;
+import java.io.File;
+/**
+ *
+ * @author julian Tanausu
+ */
+public class getName {
+    public static void main(String[] args) {
+      
+        File f = null;
+        File f1 = null;
+        String v, v1;
+        boolean bool = false;
+      
+        try{
+            // create new files
+            f = new File("C:\\test.txt");
+            f1 = new File("C:\\Program Files");
+         
+            // get file name or directory name
+            v = f.getName();
+            v1 = f1.getName();
+         
+            // true if the file path exists
+            bool = f.exists();
+         
+            // if file exists
+            if(bool){
+                // prints
+                System.out.println("File name: "+v);
+            }
+         
+            // true if the directory exists
+            bool = f1.exists();
+         
+            // if the folder exists
+            if(bool){
+                // prints
+                System.out.print("Folder name: "+v1);
+            }
+        }catch(Exception e){
+            // if any error occurs
+            e.printStackTrace();
+      }
+   }
+}
