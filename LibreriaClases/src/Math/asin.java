d6ea1de0d18d4f845597ba5cbe026b101095c5ea
alu0100727296 <alu0100727296@ull.edu.es>
2016-02-11 19:13:34 +0000

    "asin.java funcionando"

---
+++ LibreriaClases/src/Math/asin.java
@@ -0,0 +1,19 @@
+package Math;
+import java.lang.Math;
+/**
+ *
+ * @author julian Tanausu
+ */
+public class asin {
+    public static void main(String[] args) {
+        
+
+        double d2 = 0.7;
+        int i2 = 2;
+        int i3 = 1;
+
+
+        System.out.println("|" + d2 + "| es " + Math.asin(d2));
+        System.out.println("|" + i2 + "| es " + Math.asin(i3));
+    }
+}
