5cc3f414745bb2bfd0468b874044f0790eca5064
alu0100727296 <alu0100727296@ull.edu.es>
2016-02-11 20:06:32 +0000

    "cos.java funcionando"

---
+++ LibreriaClases/src/Math/cos.java
@@ -0,0 +1,17 @@
+package Math;
+import java.lang.Math;
+/**
+ *
+ * @author julian Tanausu
+ */
+public class cos {
+    public static void main(String[] args) {
+        
+        double d1 = -2.55;
+	double d2 = 0.7;
+		
+        
+        System.out.println("|" + d2 + "| es " + Math.cos(d2));
+	System.out.println("|" + d1 + "| es " + Math.cos(d1));
+    }
+}
