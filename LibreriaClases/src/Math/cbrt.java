f4973eb055ed494ab4032d5bd55e392fc1760380
alu0100727296 <alu0100727296@ull.edu.es>
2016-02-11 19:47:08 +0000

    "cbrt.java funcionando"

---
+++ LibreriaClases/src/Math/cbrt.java
@@ -0,0 +1,18 @@
+package Math;
+import java.lang.Math;
+/**
+ *
+ * @author julian Tanausu
+ */
+public class cbrt {
+    public static void main(String[] args) {
+ 
+        double d2 = 0.7;
+
+        int i2 = 2;
+        
+        
+        System.out.println("|" + d2 + "| es " + Math.cbrt(d2));
+        System.out.println("|" + i2 + "| es " + Math.cbrt(i2));
+    }
+}
