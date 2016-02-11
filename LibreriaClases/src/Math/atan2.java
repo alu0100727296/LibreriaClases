2d920e86338f2a27bb101db13d5eb68296d34f45
alu0100727296 <alu0100727296@ull.edu.es>
2016-02-11 19:38:18 +0000

    "atan2.java funcionando"

---
+++ LibreriaClases/src/Math/atan2.java
@@ -0,0 +1,21 @@
+package Math;
+import java.lang.Math;
+/**
+ *
+ * @author julian Tanausu
+ */
+public class atan2 {
+    public static void main(String[] args) {
+        double d1 = -2.55;
+	double d2 = 0.7;
+
+	
+	int i1 = -5;
+	int i2 = 2;
+	int i3 = 1;
+		
+		
+	System.out.println("|" + d2 + "| es " + Math.atan2(d2,d1));
+	System.out.println("|" + i2 + "| es " + Math.atan2(i3,i1));
+    }
+}
