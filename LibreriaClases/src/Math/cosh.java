a384d60438afc7ba69bf70480c9d8387406ba5b0
alu0100727296 <alu0100727296@ull.edu.es>
2016-02-11 20:14:45 +0000

    "cosh.java funcionando"

---
+++ LibreriaClases/src/Math/cosh.java
@@ -0,0 +1,17 @@
+package Math;
+import java.lang.Math;
+/**
+ *
+ * @author julian Tanausu
+ */
+public class cosh {
+    public static void main(String[] args) {
+        double d1 = -2.55;
+        double d2 = 0.7;
+		
+        
+        
+        System.out.println("|" + d2 + "| es " + Math.cosh(d2));
+	System.out.println("|" + d1 + "| es " + Math.cosh(d1));
+    }
+}
