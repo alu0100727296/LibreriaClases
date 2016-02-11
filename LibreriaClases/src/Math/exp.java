daca1d41b5a6ef4d029fed8f4ffb870b63b1e3b9
alu0100727296 <alu0100727296@ull.edu.es>
2016-02-11 20:18:34 +0000

    "exp.java funcionando"

---
+++ LibreriaClases/src/Math/exp.java
@@ -0,0 +1,16 @@
+package Math;
+import java.lang.Math;
+/**
+ *
+ * @author julian Tanausu
+ */
+public class exp {
+    public static void main(String[] args) {
+        double d1 = -2.55;
+        double d2 = 0.7;
+        
+        
+        System.out.println("|" + d2 + "| es " + Math.exp(d2));
+	System.out.println("|" + d1 + "| es " + Math.exp(d1));
+    }
+}
