5e2b240d40c7ff4bd9c24612e994cb23da5e8135
alu0100727296 <alu0100727296@ull.edu.es>
2016-02-11 19:33:09 +0000

    "atan.java funcionando"

--- LibreriaClases/src/Math/atan.java
+++ LibreriaClases/src/Math/atan.java
@@ -12,6 +12,15 @@ package Math;
 public class atan {
     public static void main(String[] args) {
         
+        double d2 = 0.7;
+ 
+        
+        int i2 = 2;
+        int i3 = 1;
+
+      
+        System.out.println("|" + d2 + "| es " + Math.atan(d2));
+        System.out.println("|" + i2 + "| es " + Math.atan(i3));
     }
     
 }
