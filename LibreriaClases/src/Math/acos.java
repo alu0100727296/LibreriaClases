565e5ea20187232b8bc1062b491ec84fe78b3176
alu0100727296 <alu0100727296@ull.edu.es>
2016-02-11 14:05:46 +0000

    "acos.java funcionando"

---
+++ LibreriaClases/src/Math/acos.java
@@ -0,0 +1,22 @@
+package Math;
+
+/**
+ *
+ * @author julian Tanausu
+ */
+public class acos {
+    public static void main(String[] args) {
+        
+        double d2 = 0.7;
+	int i2 = 2;
+	
+        
+        System.out.println("|" + d2 + "| es " + Math.acos(d2));
+	System.out.println("|" + i2 + "| es " + Math.acos(i2));
+    }
+    
+    
+    
+    
+    
+}
