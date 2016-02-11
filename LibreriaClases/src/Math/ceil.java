d54afb712f4b94bbcc9d6fa45ff72c6456a251c1
alu0100727296 <alu0100727296@ull.edu.es>
2016-02-11 19:53:45 +0000

    "ceil.java funcionando"

---
+++ LibreriaClases/src/Math/ceil.java
@@ -0,0 +1,16 @@
+package Math;
+import java.lang.Math;
+/**
+ *
+ * @author julian Tanausu
+ */
+public class ceil {
+    public static void main(String[] args) {
+        double d1 = -2.55;
+        double d3 = 1;
+
+
+        System.out.println("|" + d3 + "| es " + Math.ceil(d3));
+	System.out.println("|" + d1 + "| es " + Math.ceil(d1));
+    }
+}
