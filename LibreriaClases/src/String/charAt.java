35e26452e268d75823e394ab75022b34c19bb20c
Julian Tanusu <Julian Tanusu@DESKTOP-RNOT6V0>
2016-02-16 14:35:49 +0000

    "charAt.java funcionando"

---
+++ LibreriaClases/src/String/charAt.java
@@ -0,0 +1,22 @@
+package String;
+
+/**
+ *
+ * @author Julian Tanusu
+ */
+public class charAt {
+    public static void main(String[] args) {
+  
+        String str = "This is tutorialspoint";
+                   
+        // prints character at 1st location
+        System.out.println(str.charAt(0));
+          
+        // prints character at 5th location i.e white-space character
+        System.out.println(str.charAt(4));
+          
+        // prints character at 18th location 
+        System.out.println(str.charAt(17));
+   }
+} 
+
