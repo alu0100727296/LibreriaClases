c4c57fbcc29be7d9ef6801fe8d615d4185fdc310
Julian Tanusu <Julian Tanusu@DESKTOP-RNOT6V0>
2016-02-16 21:15:23 +0000

    "hasNext.java funcionando"

---
+++ LibreriaClases/src/Iterator/hasNext.java
@@ -0,0 +1,36 @@
+
+package Iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+/**
+ *
+ * @author Julian Tanusu
+ */
+public class hasNext {
+ public static void main(String args[]) {
+      // Create an array list
+      ArrayList al = new ArrayList();
+      // add elements to the array list
+      al.add("C");
+      al.add("A");
+      al.add("E");
+      al.add("B");
+      al.add("D");
+      al.add("F");
+
+      // Use iterator to display contents of al
+      System.out.print("Original contents of al: ");
+      Iterator itr = al.iterator();
+      while(itr.hasNext()) {
+         Object element = itr.next();
+         System.out.print(element + " ");
+      }
+      System.out.println();
+      
+	
+       
+    }
+}
