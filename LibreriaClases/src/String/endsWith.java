7866ba12455380f2deca576eaa69c770eb95b0c5
Julian Tanusu <Julian Tanusu@DESKTOP-RNOT6V0>
2016-02-16 15:22:23 +0000

    "endsWith.java funcionando"

---
+++ LibreriaClases/src/String/endsWith.java
@@ -0,0 +1,25 @@
+package String;
+import java.lang.*;
+/**
+ *
+ * @author Julian Tanusu
+ */
+public class endsWith {
+    public static void main(String[] args) {
+  
+   String str = "www.tutorialspoint.com";
+   System.out.println(str);
+
+   // the end string to be checked
+   String endstr1 = ".com";
+   String endstr2 = ".org";
+
+   // checks that string str ends with given substring
+   boolean retval1 = str.endsWith(endstr1);
+   boolean retval2 = str.endsWith(endstr2);
+
+   // prints true if the string ends with given substring
+   System.out.println("ends with " + endstr1 + " ? " + retval1);
+   System.out.println("ends with " + endstr2 + " ? " + retval2);
+   }
+}
