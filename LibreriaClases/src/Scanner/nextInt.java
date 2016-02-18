4691e88153c25f1f4c56a1efb78ed24ee2242f0e
Julian Tanusu <Julian Tanusu@DESKTOP-RNOT6V0>
2016-02-18 13:13:58 +0000

    "nextInt.java modificado y funcionando"

---
+++ LibreriaClases/src/Scanner/nextInt.java
@@ -0,0 +1,14 @@
+package Scanner;
+import java.util.Scanner;
+/**
+ *
+ * @author Julian Tanusu
+ */
+public class nextInt {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        x = x *2;
+        System.out.println(x);
+    }
+}
