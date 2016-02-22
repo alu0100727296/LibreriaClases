a9641107acd76df50dc0f7d399f32c2efc92e22a
Julian Tanusu <Julian Tanusu@DESKTOP-RNOT6V0>
2016-02-22 16:22:59 +0000

    "Random.java funcionando"

---
+++ LibreriaClases/src/RandomAccesFile/Random.java
@@ -0,0 +1,61 @@
+package RandomAccesFile;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.channels.OverlappingFileLockException;
+
+/**
+ *
+ * @author Julian Tanusu
+ */
+public class Random {
+    public static void main(String[] args) {
+                //La variable 'pos' indicará la posición del puntero del fichero,
+        //  señalando el lugar donde se leerá o escribirá
+        int pos = 5;
+        String nombreFichero = "prueba.txt";
+        RandomAccessFile raf = null;
+        try
+        {
+            //Se abre el fichero para permitir lectura y escritura
+            raf = new RandomAccessFile(nombreFichero,"rw");
+            //Obtener el tamaño del fichero
+            long tamaño = raf.length();
+            //Posicionar el puntero del fichero en la posición indicada
+            raf.seek(pos);
+            //Comprobar si se intenta posicionar el puntero mas allá
+            //  del tamaño del fichero
+            if(raf.getFilePointer()<tamaño)            {
+                //Leer un carácter y avanzar puntero al siguiente carácter
+                //Los caracteres leídos se toman de tipo entero
+                int caract = raf.read();
+                //Retroceder una posición para volver a la posición anterior
+                raf.seek(raf.getFilePointer()-1);
+                //Escribir el carácter leído convertido a mayúsculas
+                raf.write(Character.toUpperCase(caract));
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Error: Fichero no encontrado");
+            System.out.println(e.getMessage());
+        }
+        catch(Exception e){
+            System.out.println("Error de lectura/escritura en el fichero");
+            System.out.println(e.getMessage());
+        }
+        finally {
+            try {
+                if(raf != null)
+                    raf.close();
+            }
+            catch (Exception e) {
+                System.out.println("Error al cerrar el fichero");
+                System.out.println(e.getMessage());
+            }
+        
+    }
+}
+}
